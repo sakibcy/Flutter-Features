62de83b7ed5e2535edc030bed1b8be67000110c8
sakibcy <sakibal623@gmail.com>
2022-04-25 17:30:18 +0600

    added ButtonBar

--- lib/screens/tab_cart.dart
+++ lib/screens/tab_cart.dart
@@ -19,81 +19,93 @@ class _TabCartState extends State<TabCart> {
         padding: const EdgeInsets.all(30.0),
         child: Row(
           children: [
-            Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  const ElevatedButton(
-                    onPressed: null,
-                    child: Text('Disabled'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('Add to Cart'),
-                  ),
-                  OutlinedButton(
-                    onPressed: () {},
-                    child: const Text('Outlined Button'),
-                  ),
-                  const SizedBox(height: 20),
-                  Row(
-                    children: [
-                      Radio<int>(
-                          value: 1,
-                          groupValue: _character,
-                          onChanged: (value) {
-                            setState(() {
-                              _character = value!;
-                            });
-                          }),
-                      const Text('Radio 1'),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Radio<int>(
-                          value: 2,
-                          groupValue: _character,
-                          onChanged: (value) {
-                            setState(() {
-                              _character = value!;
-                            });
-                          }),
-                      const Text('Radio 2'),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Switch(
-                          value: _switchChange,
-                          onChanged: (value) {
-                            setState(() {
-                              _switchChange = value;
-                            });
-                          }),
-                      const Text('Switch'),
-                    ],
-                  ),
-                  Wrap(
-                    spacing: 8.0,
-                    runSpacing: 4.0,
-                    children: const [
-                      Chip(
-                          backgroundColor: Colors.purple,
-                          avatar: CircleAvatar(
-                            child: Icon(Icons.done, color: Colors.white),
-                            backgroundColor: Colors.purple,
-                          ),
-                          label: Text(
-                            'Chip',
-                            style: TextStyle(color: Colors.white),
-                          )),
-                      Chip(
-                        avatar: Icon(Icons.done),
-                        label: Text('Chip 2'),
+            Column(crossAxisAlignment: CrossAxisAlignment.center, children: <
+                Widget>[
+              const ElevatedButton(
+                onPressed: null,
+                child: Text('Disabled'),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Add to Cart'),
+              ),
+              OutlinedButton(
+                onPressed: () {},
+                child: const Text('Outlined Button'),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  Radio<int>(
+                      value: 1,
+                      groupValue: _character,
+                      onChanged: (value) {
+                        setState(() {
+                          _character = value!;
+                        });
+                      }),
+                  const Text('Radio 1'),
+                ],
+              ),
+              Row(
+                children: [
+                  Radio<int>(
+                      value: 2,
+                      groupValue: _character,
+                      onChanged: (value) {
+                        setState(() {
+                          _character = value!;
+                        });
+                      }),
+                  const Text('Radio 2'),
+                ],
+              ),
+              Row(
+                children: [
+                  Switch(
+                      value: _switchChange,
+                      onChanged: (value) {
+                        setState(() {
+                          _switchChange = value;
+                        });
+                      }),
+                  const Text('Switch'),
+                ],
+              ),
+              Wrap(
+                spacing: 8.0,
+                runSpacing: 4.0,
+                children: const [
+                  Chip(
+                      backgroundColor: Colors.purple,
+                      avatar: CircleAvatar(
+                        child: Icon(Icons.done, color: Colors.white),
+                        backgroundColor: Colors.purple,
                       ),
-                    ],
-                  )
-                ]),
+                      label: Text(
+                        'Chip',
+                        style: TextStyle(color: Colors.white),
+                      )),
+                  Chip(
+                    avatar: Icon(Icons.done),
+                    label: Text('Chip 2'),
+                  ),
+                ],
+              ),
+              Container(
+                color: Colors.grey,
+                child: ButtonBar(
+                  alignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    IconButton(onPressed: () {}, icon: const Icon(Icons.map)),
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.airport_shuttle)),
+                    IconButton(onPressed: () {}, icon: const Icon(Icons.brush)),
+                  ],
+                ),
+              )
+            ]),
             Column(
               children: [
                 PopupMenuButton(
